3ffa91fd92f958d1ea85b0edae3997c82732c7aa
Diaspro3000 <145842444+Diaspro3000@users.noreply.github.com>
2023-11-07 17:43:32 +0600

    Add files via upload

---
+++ Flask-main/calc/2.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def do_math():
+    a = request.form.get('a')
+    b = request.form.get('b')
+    operator = request.form.get('operator')
+    result = 0
+    if operator == '+':
+        result = int(a) + int(b)
+    elif operator == '-':
+        result = int(a) - int(b)
+    elif operator == '*':
+        result = int(a) * int(b)
+    elif operator == '/':
+        if int(b) == 0:
+            result = 'unpropriate operation'
+            return render_template('index.html', the_result = result)
+        result = int(a) / int(b)
+    
+    return render_template('index.html', the_result = result)
+
+app.run(debug= True)
